339fb57e53c913cc5ac3052f2296322680dec9a7
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-06-08 17:35:02 +0200

    Revert the CORS filter

--- rest-web-service/src/main/java/com/gnd/parking/ParkingServer.java
+++ rest-web-service/src/main/java/com/gnd/parking/ParkingServer.java
@@ -1,26 +1,9 @@
 package com.gnd.parking;
 
-import org.jboss.resteasy.plugins.interceptors.CorsFilter;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 @ApplicationPath("/")
 public class ParkingServer extends Application {
-    private Set<Object> singletons = new HashSet<>();
-
-    ParkingServer() {
-        CorsFilter corsFilter = new CorsFilter();
-        corsFilter.getAllowedOrigins().add("*");
-        corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
-
-        singletons.add(corsFilter);
-    }
-
-    @Override
-    public Set<Object> getSingletons() {
-        return singletons;
-    }
+    //
 }
