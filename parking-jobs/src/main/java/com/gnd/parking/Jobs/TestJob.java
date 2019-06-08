3244b61b6fa6e8328a15b8836e90602e267ab7ce
Grzegorz Tłuszcz <gtluszcz@gmail.com>
2019-06-08 17:10:11 +0200

    add test with parameter

--- parking-jobs/src/main/java/com/gnd/parking/Jobs/TestJob.java
+++ parking-jobs/src/main/java/com/gnd/parking/Jobs/TestJob.java
@@ -1,13 +1,23 @@
 package com.gnd.parking.Jobs;
 
+import com.gnd.parking.Contracts.Jobs.TestJobInterface;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
 @Stateless
-@Remote(Runnable.class)
-public class TestJob implements Runnable {
+@Remote(TestJobInterface.class)
+public class TestJob implements TestJobInterface {
+
+    private String param;
+
+    @Override
+    public void setParam(String lol) {
+        this.param = lol;
+    }
+
     @Override
     public void run() {
-        System.out.println("test run");
+        System.out.println(this.param);
     }
 }
--- soap-web-service/src/main/java/com/gnd/parking/HelloWorld.java
+++ soap-web-service/src/main/java/com/gnd/parking/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.gnd.parking;
 
+import com.gnd.parking.Contracts.Jobs.TestJobInterface;
 import com.gnd.parking.Contracts.Services.Scheduling.ParkingSchedulerServiceInterface;
 
 import javax.ejb.EJB;
@@ -11,13 +12,14 @@ import java.util.concurrent.TimeUnit;
 public class HelloWorld {
 
     @EJB(lookup = "java:global/parking-jobs-1.0/TestJob")
-    Runnable testJob;
+    TestJobInterface testJob;
 
     @EJB(lookup = "java:global/parking-jobs-1.0/ParkingSchedulerService")
     ParkingSchedulerServiceInterface parkingSchedulerService;
 
     @WebMethod
     public String sayHelloWorldFrom(String from) {
+        testJob.setParam("omg");
         parkingSchedulerService.schedule(testJob, 5, TimeUnit.SECONDS);
         return "Hello, world, from " + from;
     }
