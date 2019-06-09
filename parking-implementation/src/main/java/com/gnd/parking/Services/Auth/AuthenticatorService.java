b0f28f66a6e7c9634be059936e380aa3ff51ad02
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-06-09 15:39:05 +0200

    Added ability to change passwords in the domain code

--- parking-implementation/src/main/java/com/gnd/parking/Services/Auth/AuthenticatorService.java
+++ parking-implementation/src/main/java/com/gnd/parking/Services/Auth/AuthenticatorService.java
@@ -2,6 +2,7 @@ package com.gnd.parking.Services.Auth;
 
 import com.gnd.parking.Contracts.Repositories.UsersRepositoryInterface;
 import com.gnd.parking.Contracts.Services.Auth.AuthenticatorServiceInterface;
+import com.gnd.parking.Exceptions.NestedObjectNotFoundException;
 import com.gnd.parking.Models.User;
 import org.mindrot.jbcrypt.BCrypt;
 
@@ -15,6 +16,7 @@ public class AuthenticatorService implements AuthenticatorServiceInterface {
     @EJB
     UsersRepositoryInterface usersRepository;
 
+    @Override
     public boolean authenticate(String username, String password) {
         User user = usersRepository.find(username);
 
@@ -25,5 +27,29 @@ public class AuthenticatorService implements AuthenticatorServiceInterface {
         String hashedPassword = user.getPassword();
 
         return BCrypt.checkpw(password, hashedPassword);
+    }
+
+    @Override
+    public boolean changePassword(User user, String newPassword) {
+        user.setPassword(BCrypt.hashpw(newPassword, BCrypt.gensalt()));
+
+        try {
+            usersRepository.update(user);
+
+            return true;
+        } catch (NestedObjectNotFoundException e) {
+            throw new RuntimeException("This should not happen, as there was no change in region.");
+        }
+    }
+
+    @Override
+    public boolean changePassword(User user, String oldPassword, String newPassword) {
+        boolean valid = BCrypt.checkpw(oldPassword, user.getPassword());
+
+        if (!valid) {
+            return false;
+        }
+
+        return changePassword(user, newPassword);
     }
 }
