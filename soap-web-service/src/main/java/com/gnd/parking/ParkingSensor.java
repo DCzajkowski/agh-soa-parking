bafd7f6ce70e48c7b98b2c1d76584b387de95fda
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-06-12 13:27:52 +0200

    SOAP authentication

--- soap-web-service/src/main/java/com/gnd/parking/ParkingSensor.java
+++ soap-web-service/src/main/java/com/gnd/parking/ParkingSensor.java
@@ -1,15 +1,28 @@
 package com.gnd.parking;
 
+import com.gnd.parking.Contracts.Services.Auth.AuthenticatorServiceInterface;
 import com.gnd.parking.Contracts.Services.ParkingSpots.Exceptions.ParkingSpotException;
 import com.gnd.parking.Contracts.Services.ParkingSpots.ParkingSpotReleaserServiceInterface;
 import com.gnd.parking.Contracts.Services.ParkingSpots.ParkingSpotTakerServiceInterface;
 
+import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.naming.AuthenticationException;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+import java.util.List;
+import java.util.Map;
 
 @WebService
 public class ParkingSensor {
+
+    @Resource
+    WebServiceContext webServiceContext;
+
+    @EJB(lookup = "java:global/parking-logic-1.0/AuthenticatorService")
+    AuthenticatorServiceInterface authenticatorService;
 
     @EJB(lookup = "java:global/parking-logic-1.0/ParkingSpotTakerService")
     ParkingSpotTakerServiceInterface parkingSpotTaker;
@@ -18,14 +31,43 @@ public class ParkingSensor {
     ParkingSpotReleaserServiceInterface parkingSpotReleaser;
 
     @WebMethod
-    public String takeParkingSpot(Integer spotId) throws ParkingSpotException {
+    public boolean takeParkingSpot(Integer spotId) throws ParkingSpotException, AuthenticationException {
+        authorize();
+
         parkingSpotTaker.takeParkingSpot(spotId);
-        return "success";
+
+        return true;
     }
 
     @WebMethod
-    public String releaseParkingSpot(Integer spotId) throws ParkingSpotException {
+    public boolean releaseParkingSpot(Integer spotId) throws ParkingSpotException, AuthenticationException {
+        authorize();
+
         parkingSpotReleaser.releaseParkingSpot(spotId);
-        return "success";
+
+        return true;
+    }
+
+    public void authorize() throws AuthenticationException {
+        MessageContext messageContext = webServiceContext.getMessageContext();
+
+        Map<?, ?> requestHeaders = (Map<?, ?>) messageContext.get(MessageContext.HTTP_REQUEST_HEADERS);
+        List<?> usernameList = (List<?>) requestHeaders.get("username");
+        List<?> passwordList = (List<?>) requestHeaders.get("password");
+
+        String username = "";
+        String password = "";
+
+        if (usernameList != null) {
+            username = usernameList.get(0).toString();
+        }
+
+        if (passwordList != null) {
+            password = passwordList.get(0).toString();
+        }
+
+        if (!authenticatorService.authenticate(username, password)) {
+            throw new AuthenticationException("The username password combo is not correct.");
+        }
     }
 }
